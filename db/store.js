b9eba60ec79e2d502f525a7f71f74fa5e06c3dff
brittanymy <brittany.young2001@gmail.com>
2022-06-07 09:12:19 -0400

    created a function to read, write, get and add notes

---
+++ db/store.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const util = require("util");
+const uuid = require("../helpers/uuid");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+class Store {
+    // Read notes
+    read() {
+        return readFileAsync("db/db.json", "utf-8");
+    }
+    // Write notes
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note));
+    }
+    // Get notes
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (error) {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        });
+    }
+
+    // Add notes
+    addNotes(note) {
+        const { title, text } = note;
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+        return this.getNotes()
+            .then((notes) => [...notes, newNote])
+            .then((updatedNotes) => this.write(updatedNotes))
+            .then(() => newNote);
+    }
+}
+
+module.exports = new Store();
